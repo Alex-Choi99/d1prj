631ce66ef97590fb78de3a9303ec694336a2ad4d
Alex Choi <sequoia.alex.ca@gmail.com>
2025-11-28 11:47:56 -0800

    minor fix

--- client/js/SignUpFormHandler.js
+++ client/js/SignUpFormHandler.js
@@ -123,8 +123,8 @@ class SignUpFormHandler extends FormHandler {
             };
 
             try {
-                const response = await fetch('http://localhost:3001/signup', {
-                // const response = await fetch('https://d1prj.onrender.com/signup', {
+                // const response = await fetch('http://localhost:3001/signup', {
+                const response = await fetch('https://d1prj-server.onrender.com/signup', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(payload)
